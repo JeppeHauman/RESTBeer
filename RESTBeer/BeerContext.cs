af9adaab5dcd52be616db69cbb074de22991c43f
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-10 14:30:42 +0100

    Maybe you all know my password now

--- RESTBeer/BeerContext.cs
+++ RESTBeer/BeerContext.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Opgave_1;
 
 namespace RESTBeer
 {
-    public class BeerContext
+    public class BeerContext : DbContext
     {
+        public BeerContext(DbContextOptions<BeerContext> options) : base(options)
+        {}
+        public DbSet<Beer> Beers { get; set; }
     }
 }
--- RESTBeer/Secrets.cs
+++ RESTBeer/Secrets.cs
@@ -2,6 +2,6 @@
 {
     internal class Secrets
     {
-        public string ConnectionString = "Server=tcp:andetsemesterprojekt.database.windows.net,1433;Initial Catalog=ZZDBv2;Persist Security Info=False;User ID=mikemadeleinejeppe;Password=2E2020roskilde;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        public const string ConnectionString = "Server=tcp:andetsemesterprojekt.database.windows.net,1433;Initial Catalog=ZZDBv2;Persist Security Info=False;User ID=mikemadeleinejeppe;Password=2E2020roskilde;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     }
 }
