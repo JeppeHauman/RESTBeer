7f2ae4c2bdffdd149df224dccb8591d1bec66b6c
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-10 14:33:29 +0100

    likjsdf

--- RESTBeer/Startup.cs
+++ RESTBeer/Startup.cs
@@ -25,7 +25,7 @@ namespace RESTBeer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BeerContext>(opt => opt.UseSqlServer(Secrets.ConnectionString))
+            services.AddDbContext<BeerContext>(opt => opt.UseSqlServer(Secrets.ConnectionString));
             services.AddControllers();
         }
 
